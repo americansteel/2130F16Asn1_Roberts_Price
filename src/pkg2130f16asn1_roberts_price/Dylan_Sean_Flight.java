94e97303dc9d3983cc25835c9dfec34a512d7916
Dylan <Dylan.Roberts@georgebrown.ca>
2016-10-04 17:11:46 -0400

    project files
    first commit

---
+++ src/pkg2130f16asn1_roberts_price/Dylan_Sean_Flight.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg2130f16asn1_roberts_price;
+
+/**
+ *
+ * @author Dylan
+ */
+public class Dylan_Sean_Flight {
+    private String flightNumber;
+    private String flightSource;
+    private String flightDestination;
+    private String travelDate;
+    private double flightFare;
+    private final double TAX = 1.13;
+}
---
+++ src/pkg2130f16asn1_roberts_price/Dylan_Sean_FrequentFlyer.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg2130f16asn1_roberts_price;
+
+/**
+ *
+ * @author Dylan
+ */
+public class Dylan_Sean_FrequentFlyer {
+    private int flyerMiles;
+    private int flyerNumber;
+}
