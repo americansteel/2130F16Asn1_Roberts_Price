cef25bd699e06cd8c36192dea13c9a04737183d2
Sean <Sean@76.9.194.39>
2016-10-05 14:58:34 -0400

    Third Commit - Passenger class should be finished unless I need to add anything in order to make the subclass work (Frequent Flyer)

--- src/pkg2130f16asn1_roberts_price/Dylan_Sean_Passenger.java
+++ src/pkg2130f16asn1_roberts_price/Dylan_Sean_Passenger.java
@@ -5,13 +5,72 @@
  */
 package pkg2130f16asn1_roberts_price;
 
+import java.util.Scanner;
+
 /**
  *
- * @author Dylan
+ * @author Sean
  */
 public class Dylan_Sean_Passenger {
-    private String firstName;
-    private String lastName;
-    private int passengerAge;
+
+// Initialize variables. Need to be public in order to be read by the subclass
+    java.util.Scanner input = new Scanner(System.in);
+    public String firstName;
+    public String lastName;
+    public int passengerAge;
+ 
+/*****
+* Constructors
+******/
+    
+// Default constructor    
+    public Dylan_Sean_Passenger(){
+        firstName=lastName="";
+        passengerAge=0;
+    }
+    
+// First, Last, Age
+    public Dylan_Sean_Passenger(String fName, String lName, int pAge){
+        firstName = fName;
+        lastName = lName;
+        passengerAge = pAge;
+    }
+// First, Last 
+    public Dylan_Sean_Passenger(String fName, String lName){
+        firstName = fName;
+        lastName = lName;
+    }
+// First Only   
+    public Dylan_Sean_Passenger(String fName){
+        firstName = fName;
+    }
+    
+/*****
+* Get and Set for each   
+******/
+    public String getFName(){
+        return firstName;
+    }
+    
+    public void setFName(String fName){
+        firstName = fName;
+    }
+    
+    public String getLName(){
+        return lastName;
+    }
+    
+    public void setLName(String lName){
+        lastName = lName;
+    }
+    
+    public int getAge(){
+        return passengerAge;
+    }
+    
+    public void setAge(int age){
+        passengerAge = age;
+    }
+    
     
 }
