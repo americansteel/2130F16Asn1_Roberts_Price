fe40d58ad299e5474c225489ada0ad9217c9d8b4
Sean Price <SeanParade@users.noreply.github.com>
2016-10-08 13:56:27 -0400

    Update Dylan_Sean_FrequentFlyer.java

--- src/pkg2130f16asn1_roberts_price/Dylan_Sean_FrequentFlyer.java
+++ src/pkg2130f16asn1_roberts_price/Dylan_Sean_FrequentFlyer.java
@@ -10,21 +10,34 @@ package pkg2130f16asn1_roberts_price;
  * @author Sean
  */
 public class Dylan_Sean_FrequentFlyer extends Dylan_Sean_Passenger {
+//  Initialize variables. 
     private int flyerMiles;
     private int flyerNumber;
-    
-    public Dylan_Sean_FrequentFlyer(){
-        
+ 
+/*
+**********
+    Constructors
+**********
+*/
+
+//  Empty 
+    public Dylan_Sean_FrequentFlyer(){   
     }
-    
+ 
+// Full 
     public Dylan_Sean_FrequentFlyer(int flyMiles, int flyNum, String fName, String lName, int pAge){
+        super(fName, lName, pAge);
         flyerMiles = flyMiles;
         flyerNumber = flyNum;
-        firstName = fName;
-        lastName = lName;
-        passengerAge = pAge;
     }
     
+/*
+**********
+    Get and Set 
+**********    
+*/
+
+// Flyer Miles
     public void setFMiles(int mi){
         flyerMiles = mi;
     }
@@ -32,18 +45,49 @@ public class Dylan_Sean_FrequentFlyer extends Dylan_Sean_Passenger {
     public int getFMiles(){
         return flyerMiles;
     }
-    
+//Flyer Number
     public void setFNumber(int num){
         flyerNumber = num;
     }
-    
+
     public int getFNumber(){
         return flyerNumber;
     }
-    
+
+/*
+**********
+    Methods
+**********
+*/
+
+//  toString
     @Override
     public String toString(){
         return super.toString() +"\nFrequent Flyer Number: "+ flyerNumber+
-                                 "\nFrequent Flyer Miles: "+ flyerMiles;
+                                 "\nFrequent Flyer Miles: "+ flyerMiles+" miles";
     }
+    
+//  Validate Flyer Number - MOVE TO MAIN
+//  While condition temporarily turns the int into a string so I can use the string length method.
+    public int getValidFlyerNumber(){
+        int result;
+        result = input.nextInt();
+        while (String.valueOf(result).length() != 10){
+            System.out.println("\nThat is incorrect. Please enter your 10-digit Frequent Flyer Number.");
+            result = input.nextInt();
+        }
+        return result;
+    }
+
+//  Validate Flyer Miles - MOVE TO MAIN
+    public int getValidFlyerMiles(){
+        int result;
+        result = input.nextInt();
+        while (result < 1){
+            System.out.println("\nThat is incorrect. Please enter a valid amount of miles.");
+            result = input.nextInt();
+        }
+        return result;
+    }
+
 }
