45ef291c7a7558ae2a43c1bd1b37db059c5e98fb
Dylan <Dylan.Roberts@georgebrown.ca>
2016-10-10 13:09:55 -0400

    Main and Flight class uploaded
    Date validation is not working

--- src/pkg2130f16asn1_roberts_price/Dylan_Sean_Flight.java
+++ src/pkg2130f16asn1_roberts_price/Dylan_Sean_Flight.java
@@ -5,15 +5,108 @@
  */
 package pkg2130f16asn1_roberts_price;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
 /**
  *
  * @author Dylan
  */
 public class Dylan_Sean_Flight {
+
     private String flightNumber;
     private String flightSource;
     private String flightDestination;
     private String travelDate;
     private double flightFare;
-    private final double TAX = 1.13;
+    private final double TAX = 0.13;
+    private double subtotal;
+    private String strSubtotal;
+    private double total;
+    private String strTotal;
+
+//Default Constructor
+    public Dylan_Sean_Flight() {
+
+        flightNumber = "";
+        flightSource = "";
+        flightDestination = "";
+        //Format needs to be determined
+        travelDate = "";
+        flightFare = 0;
+    }    
+//Constructor with all 5 arguments 
+
+    public Dylan_Sean_Flight(String number, String source, String destination, String date, double fare) {
+        flightNumber = number;
+        flightSource = source;
+        flightDestination = destination;
+        travelDate = date;
+        flightFare = fare;
+        subtotal = fare * TAX;
+        total = subtotal + fare;
+        NumberFormat cf = NumberFormat.getCurrencyInstance();
+        strSubtotal = cf.format(subtotal);
+        strTotal = cf.format(total);
+    }
+//Method for getting flight number
+
+    public String getFlightNumber() {
+        return flightNumber;
+    }
+//Set flight number
+
+    public void setFlightNumber(String number) {
+        flightNumber = number;
+    }
+//Method for getting flight source
+
+    public String getFlightSource() {
+        return flightSource;
+    }
+//Set flight source
+
+    public void setFlightSource(String source) {
+        flightSource = source;
+    }
+//Method for getting flight destination
+
+    public String getFlightDestination() {
+        return flightDestination;
+    }
+//Set travel destination 
+
+    public void setFlightDestination(String destination) {
+        flightDestination = destination;
+    }
+//Get travel date
+
+    public String getTravelDate() {
+        return travelDate;
+    }
+//Set travel date
+
+    public void setTravelDate(String date) {
+        travelDate = date;
+    }
+//Get flight price
+
+    public double getFlightFare() {
+        return flightFare;
+    }
+//Set flight price
+
+    public void setFlightFare(double fare) {
+        flightFare = fare;
+    }
+    
+    @Override
+    public String toString() {
+        return "Flight Number: " + flightNumber + "\nFlight Source: " + flightSource
+                + "\nFlight Destination: " + flightDestination + "\nTravel Date: " + travelDate
+                + "\nPrice of Flight - Subotal: " + flightFare + "\n Tax: " + strSubtotal +
+                "\nTotal Price: " + strTotal;
+                
+        
+    }
 }
--- src/pkg2130f16asn1_roberts_price/Main.java
+++ src/pkg2130f16asn1_roberts_price/Main.java
@@ -5,9 +5,15 @@
  */
 package pkg2130f16asn1_roberts_price;
 
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
+import java.text.ParseException;
+
 /**
  *
  * @author Dylan
+ * @author Sean
  */
 public class Main {
 
@@ -16,6 +22,100 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        String flightNumber;
+        String flightSource;
+        String flightDestination;
+        String travelDate;
+        double flightFare;
+        Dylan_Sean_Flight flight;
+        //Build main object and run methods to build Flight object
+        Main thisObj = new Main();
+        flightNumber = thisObj.validateFlightNumber();
+        flightSource = thisObj.validateFlightSource();
+        flightDestination = thisObj.validateFlightDestination();
+        flightFare = thisObj.validateFlightFare();
+        //Not doing anything right now
+        travelDate = thisObj.validateFlightDate();
+
+        flight = new Dylan_Sean_Flight(flightNumber, flightSource, flightDestination, travelDate, flightFare);
+        System.out.println(flight.toString());
+
     }
-    
+    //Validation method for flight number
+
+    public String validateFlightNumber() {
+        String flightNum;
+        System.out.println("Please enter your Flight Number");
+        Scanner input = new Scanner(System.in);
+        flightNum = input.nextLine();
+        while (flightNum.equals("")) {
+            System.out.println("Please enter a valid Flight Number");
+            flightNum = input.nextLine();
+        }
+        return flightNum;
+    }
+    //Validation method for flight source
+
+    public String validateFlightSource() {
+        String flightSource;
+        System.out.println("Please enter your Flight Source: ");
+        Scanner input = new Scanner(System.in);
+        flightSource = input.nextLine();
+        while (flightSource.equals("")) {
+            System.out.println("Please enter a valid location");
+            flightSource = input.nextLine();
+        }
+        return flightSource;
+    }
+    //Validation method for destination
+
+    public String validateFlightDestination() {
+        String flightDestination;
+        System.out.println("Please enter your Flight Destination");
+        Scanner input = new Scanner(System.in);
+        flightDestination = input.nextLine();
+        while (flightDestination.equals("")) {
+            System.out.println("Please enter a valid Flight Destination");
+            flightDestination = input.nextLine();
+        }
+        return flightDestination;
+    }
+    //Validation for price
+
+    public double validateFlightFare() {
+        double flightFare;
+        String fareCheck;
+        System.out.println("Please enter your Flight Fare");
+        Scanner input = new Scanner(System.in);
+        fareCheck = input.nextLine();
+        while (!fareCheck.matches("^[0-9]+(\\.[0-9]{1,2})?$")) {
+            System.out.println("Please enter a valid fare");
+            fareCheck = input.nextLine();
+        }
+
+        flightFare = Double.parseDouble(fareCheck);
+        return flightFare;
+    }
+   
+    //Validation for date
+    //This doesnt do anything right now
+    public String validateFlightDate() {
+        Scanner input = new Scanner(System.in);
+        String date;
+        boolean loop = false;
+        SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy");
+        df.setLenient(false);
+        System.out.println("Please enter a date (dd-MM-yyyy): ");
+        date = input.nextLine();
+        while (loop = false) {
+            try {
+                df.parse(date.trim());
+            } catch (Exception e) {
+                loop = false;
+                System.out.println("Please enter a valid date");
+            }
+
+        }
+        return date;
+    }
 }
